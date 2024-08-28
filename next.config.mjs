02b3966e06b329e39fbcc8d25be03a4e5d03bc3c
hm82 <hasan.mujtaba@gmail.com>
2024-08-28 13:50:36 -0500

    Enable Partial Prerendering to allow Dynamic Rend- ering of Components alongside Static rendering of other components on the same page

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: 'incremental', // enable experimental partial pre-rendering
+      },
+};
 
 export default nextConfig;
