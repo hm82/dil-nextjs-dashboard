16a7cd3f4fb33473c49e3ca08d4d36216a2c139a
hm82 <hasan.mujtaba@gmail.com>
2024-08-29 08:36:58 -0500

    Code for Chapter 14 - Error Handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
     // get Customer ID from Params
@@ -10,19 +11,24 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchInvoiceById(id),
         fetchCustomers(),
       ]);
-  return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
-          {
-            label: 'Edit Invoice',
-            href: `/dashboard/invoices/${id}/edit`,
-            active: true,
-          },
-        ]}
-      />
-      <Form invoice={invoice} customers={customers} />
-    </main>
-  );
+    
+    if (!invoice) {
+    notFound();
+    }  
+
+    return (
+        <main>
+        <Breadcrumbs
+            breadcrumbs={[
+            { label: 'Invoices', href: '/dashboard/invoices' },
+            {
+                label: 'Edit Invoice',
+                href: `/dashboard/invoices/${id}/edit`,
+                active: true,
+            },
+            ]}
+        />
+        <Form invoice={invoice} customers={customers} />
+        </main>
+    );
 }
